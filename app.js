c4d5e66968f67a9f4a07d79102e5fc263cbe97be
Shriram <shriramrayakar@gmail.com>
2023-11-14 22:07:01 +0530

    second commit

---
+++ app.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var http = require('http');
+var mongoose = require('mongoose');
+var dbUrl = 'https://ap-south-1.aws.data.mongodb-api.com/app/data-rdssz/endpoint/data/v1'
+
+const connectToMongo = async () => {
+  try {
+    mongoose.set("strictQuery", false);
+    mongoose.connect(dbUrl);
+    console.log("Connected to Mongo Successfully!");
+  } catch (error) {
+    console.log(error);
+  }
+};
+module.exports = connectToMongo;
+
+
+var app = express();
+var server = http.createServer(app);
+
+var io = require('socket.io')(server);
+var path = require('path');
+
+
+app.use(express.static(path.join(__dirname,'./public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+
+var name;
+
+io.on('connection', (socket) => {
+  console.log('new user connected');
+  
+  socket.on('joining msg', (username) => {
+  	name = username;
+  	io.emit('chat message', `---${name} joined the chat---`);
+  });
+  
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+    io.emit('chat message', `---${name} left the chat---`);
+    
+  });
+  socket.on('chat message', (msg) => {
+    socket.broadcast.emit('chat message', msg);         //sending message to all except the sender
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server listening on :3000');
+});
